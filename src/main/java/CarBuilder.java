86e31e8059f02c35cdfcfa68e35245add126059b
WoldemarK <kovtynov.vladimir@gmail.com>
2022-05-29 19:13:48 +0300

    Initial commit

---
+++ src/main/java/CarBuilder.java
@@ -0,0 +1,16 @@
+public interface CarBuilder {
+
+    CarBuilder fixChassis();
+
+    CarBuilder fixBody();
+
+
+    CarBuilder paint();
+
+
+    CarBuilder fixInterior();
+
+
+    Car build();
+}
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        CarBuilder builder = new SportsCarBuilder();
+        AutomotiveEngineer engineer = new AutomotiveEngineer(builder);
+        Car car = engineer.manufactureCar();
+        if (car != null) {
+            System.out.println("Below car delievered: ");
+            System.out.println("======================================================================");
+            System.out.println(car);
+            System.out.println("======================================================================");
+        }
+    }
+
+}
